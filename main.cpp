4470f13531759e4a9b932b41aebd0195864b74e4
Ariane Djad <arianedjad@gmail.com>
2025-06-18 23:16:04 +0100

    add demo app and translation files

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "QDiffWidget.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "QDiffX_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    QDiffWidget w;
+    w.show();
+    return a.exec();
+}
