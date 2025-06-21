e0e363c2da2fb3f010c8098bd075fe8a5147b286
Ariane Djad <arianedjad@gmail.com>
2025-06-21 12:31:32 +0100

    feat: add DMPAlgorithm class files

---
+++ src/DMPAlgorithm.cpp
@@ -0,0 +1,3 @@
+#include "DMPAlgorithm.h"
+
+DMPAlgorithm::DMPAlgorithm() {}
---
+++ src/DMPAlgorithm.h
@@ -0,0 +1,16 @@
+#ifndef DMPALGORITHM_H
+#define DMPALGORITHM_H
+#include "QDiffAlgorithm.h"
+
+namespace QDiffX{
+
+
+class DMPAlgorithm : QDiffAlgorithm
+{
+public:
+    DMPAlgorithm();
+};
+
+
+}// namespace QDiffX
+#endif // DMPALGORITHM_H
