a2c8d7ce7be150072e4079fee5a75c2bb714717a
Ariane Djad <arianedjad@gmail.com>
2025-06-24 18:43:05 +0100

    feat: implement basic singleton class structure for QAlgorithmRegistry

--- src/QAlgorithmRegistry.h
+++ src/QAlgorithmRegistry.h
@@ -1,9 +1,22 @@
 #ifndef QALGORITHMREGISTRY_H
 #define QALGORITHMREGISTRY_H
+#include "QDiffAlgorithm.h"
 
+// follows singleton pattern
 class QAlgorithmRegistry
 {
-    QAlgorithmRegistry();
+public:
+ static QAlgorithmRegistry& get_Instance();
+
+private:
+    QAlgorithmRegistry() = default;
+    ~QAlgorithmRegistry() = default;
+
+    QAlgorithmRegistry(const QAlgorithmRegistry &) = delete;
+    QAlgorithmRegistry(const QAlgorithmRegistry &&) = delete;
+    QAlgorithmRegistry operator=(const QAlgorithmRegistry &) = delete;
+    QAlgorithmRegistry operator=(const QAlgorithmRegistry &&) = delete;
+
 };
 
 #endif // QALGORITHMREGISTRY_H
