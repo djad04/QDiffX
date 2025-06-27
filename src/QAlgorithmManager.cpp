fcf4b6b3ca3c6c2d95c0f057dce12f5eca85afc9
Ariane Djad <arianedjad@gmail.com>
2025-06-27 23:33:26 +0100

    feat: implement constructor for QAlgorithmManager and static config variables

--- src/QAlgorithmManager.cpp
+++ src/QAlgorithmManager.cpp
@@ -1,3 +1,20 @@
 #include "QAlgorithmManager.h"
 
-QAlgorithmManager::QAlgorithmManager() {}
+namespace QDiffX{
+const QString QAlgorithmManager::DEFAULT_ALGORITHM = "dtl";
+const QString QAlgorithmManager::DEFAULT_FALLBACK = "dmp";
+
+
+QAlgorithmManager::QAlgorithmManager(QObject *parent)
+    : QObject(parent),
+    m_currentAlgorithm(DEFAULT_ALGORITHM),
+    m_fallBackAlgorithm(DEFAULT_FALLBACK),
+    m_selectionMode(AlgorithmSelectionMode::Auto),
+    m_executionMode(ExecutionMode::Synchronous)
+{
+
+}
+
+
+
+}//namespace QDiffX
