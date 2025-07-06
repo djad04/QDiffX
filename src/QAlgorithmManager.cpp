313f8e7fbb47c3a1ca37aadd18189f27c3e466e1
Ariane Djad <arianedjad@gmail.com>
2025-07-06 19:37:08 +0100

    refactor: streamline test structure in Tst_QAlgorithmManager and enhance algorithm registration checks

--- src/QAlgorithmManager.cpp
+++ src/QAlgorithmManager.cpp
@@ -104,7 +104,11 @@ QDiffResult QAlgorithmManager::calculateDiffSync(const QString &leftText, const 
     } else {
         algorithm = autoSelectAlgorithm(leftText, rightText);
     }
-    return executeAlgorithm(algorithm, leftText, rightText);
+    QDiffResult result = executeAlgorithm(algorithm, leftText, rightText);
+    if (result.success()) {
+        emit diffCalculated(result);
+    }
+    return result;
 }
 
 QDiffResult QAlgorithmManager::calculateDiffWithAlgorithm(const QString &algorithmId, const QString &leftText, const QString &rightText)
