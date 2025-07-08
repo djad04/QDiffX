323bee2673f4cef49eb949b61531b70ba13a781a
Ariane Djad <arianedjad@gmail.com>
2025-07-08 23:39:12 +0100

    feat:QDiffTextBrowser class explicit constructor and method declarations

--- src/QDiffTextBrowser.h
+++ src/QDiffTextBrowser.h
@@ -2,10 +2,26 @@
 
 #include <QObject>
 #include <QtWidgets/QTextBrowser>
+#include "LineNumberArea.h"
 
 class QDiffTextBrowser : public QTextBrowser
 {
+    Q_OBJECT
 public:
-    QDiffTextBrowser();
+    explicit QDiffTextBrowser(QWidget* parent = nullptr);
+
+    int lineNumberAreaWidth() const;
+    void setDiffResult(const QDiffResult& result); // To be implemented later
+
+protected:
+    void resizeEvent(QResizeEvent* event) override;
+    void scrollContentsBy(int dx, int dy) override;
+    void paintEvent(QPaintEvent* event) override;
+
+public:
+    void paintLineNumberArea(QPaintEvent* event);
+
+private:
+    LineNumberArea* m_lineNumberArea;
 };
 
